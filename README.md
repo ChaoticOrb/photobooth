298421c26cc974d77260b1d70ad198ee10c413dc
ChaoticOrb <ChaoticOrb@users.noreply.github.com>
2017-05-18 20:44:00 +0100

    Started readme and added LEDBoard to the main programme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # photobooth
-Python photo booth programme for a wedding
+Photo booth programme for use with picamera.
+
+This was put together to be used as a DIY wedding photo booth to avoid the ridiculous costs of hiring a DLSR in a box.
+
+## requirements:
+* **raspberry pi** - this was built with a [Pi Zero](https://www.raspberrypi.org/products/pi-zero-w/) W but you could easily use a Raspberry Pi 3. Having built in wifi makes set up and running it much easier.
+* **Pi Camera Module v2** - the [second version](https://www.raspberrypi.org/products/camera-module-v2/) of the Pi Camera module with a Pi Zero cable. [Read the docs](https://picamera.readthedocs.io/en/release-1.13/)
+
+## usage
+tbc
--- photobooth.py
+++ photobooth.py
@@ -1,4 +1,6 @@
 
+#!/usr/bin/env python
+
 #############################
 # photobooth.py
 # created by ChaoticOrb
@@ -6,24 +8,23 @@
 from picamera import PiCamera, Color
 from time import sleep
 from datetime import datetime
-from gpiozero import Button, LED
+from gpiozero import Button, LED, LEDBoard
 
 #############################
 # variables config
 #############################
 camera = PiCamera()
 
-
 yellow_button = Button(17) # pin for the start Button
-red = LED(27) # red led
-green = LED(22)
+# red = LED(27) # red led
+leds = LEDBoard(22, 27, 13)
 
 total_photos = 3 # total number of photos to take
-capture_delay = 1 # delay between taking photos
-countdown = 1 # countdown for each photo
-res_w = 2592 # width of camera resolution (max is 2592)
-res_h = 1944 # height of camera resolution (max is 1944)
-text_size = 140 # size of annotated text
+capture_delay = 3 # delay between taking photos
+countdown = 3 # countdown for each photo
+res_w = 800 # width of camera resolution (max is 2592)
+res_h = 480 # height of camera resolution (max is 1944)
+text_size = 80 # size of annotated text
 text_color = '#fff' # colour of annotated text
 bg_color = '#000' # colour of annotated text background
 test_server = 'www.google.com' # server location to check for network
@@ -33,13 +34,17 @@ reset_delay = 5
 #############################
 # functions
 #############################
+def fireUp():
+    # show image about pressing the button
+    yellow_button.wait_for_press()
+    print('Button pressed')
+    previewOn()
+
 def previewOn():
     camera.vflip = False # change if camera is mounted upside down
     camera.resolution = camera.MAX_RESOLUTION
-    camera.start_preview(resolution=(1024, 768))
+    camera.start_preview(resolution=(res_w, res_h))
     print('Camera preview turned on')
-    yellow_button.wait_for_press()
-    print('Button pressed')
     takePhotos()
 
 def takePhotos():
@@ -50,29 +55,42 @@ def takePhotos():
         camera.annotate_background = Color(bg_color)
         camera.annotate_text_size = text_size
         camera.annotate_text = 'Get Ready!'
-        sleep(2)
+        sleep(1)
         camera.annotate_text = 'Photos taken after {}s countdown'.format(countdown)
-        sleep(3)
+        sleep(1)
+        # photo loop starts
         for x in range(1,total_photos + 1):
             count = 3
             for i in range(1,countdown + 1):
                 camera.annotate_text = '{}...'.format(count)
                 sleep(1)
                 count = count - 1
-            print('Taking photo {} - img'.format(x) + timestamp + '-{}.jpg'.format(x))
             camera.annotate_text = ''
             red.on()
             sleep(1)
             red.off()
+            print('Taking photo {} - img'.format(x) + timestamp + '-{}.jpg'.format(x))
             camera.capture('/home/pi/captures/img' + timestamp + '-{}.jpg'.format(x))
             sleep(1)
-        green.on()
-        camera.annotate_text = 'All Done! Resetting...'
-        print('Restarting')
-        sleep(reset_delay)
-        camera.annotate_text = ''
-        green.off()
+            resetCamera() # reset camera
     finally:
         camera.close()
 
-previewOn()
+def resetCamera():
+    camera.annotate_text = 'Saving...'
+    LEDBoard(1,0,0)
+    sleep(1)
+    LEDBoard(1,1,0)
+    sleep(1)
+    LEDBoard(1,1,1)
+    leds.blink()
+    sleep(2)
+    camera.annotate_text = 'Resetting...'
+    print('Restarting')
+    sleep(reset_delay)
+    camera.annotate_text = ''
+
+#############################
+# main programme
+#############################
+fireUp()
