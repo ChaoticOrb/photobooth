2830cbd9dc873063dab81af1b16d0dcf179ed202
ChaoticOrb <ChaoticOrb@users.noreply.github.com>
2017-05-30 21:27:59 +0100

    Updated image display using pygame Will be used to display instructional messages throughout the photobooth programme

--- visual_instructions.py
+++ visual_instructions.py
@@ -6,21 +6,16 @@ from time import sleep
 pygame.init()
 res_w = 800 # width of camera resolution (max is 2592)
 res_h = 480 # height of camera resolution (max is 1944)
-screen = pygame.display.set_mode((res_w,res_h))
-pygame.mouse.set_visible(False) #hide the mouse cursor
+img = pygame.display.set_mode((res_w,res_h), pygame.FULLSCREEN)
+screen = pygame.display.get_surface()
+pygame.mouse.set_visible(False)
 
 def show_instructions():
-    surface = pygame.image.load('instructions_1.png').convert()
+    img = pygame.image.load('instructions_1.png').convert()
+    screen.blit(img,(0,0))
     pygame.display.flip() # updates the whole screen
     sleep(10)
     pygame.quit()
 
-def clear_screen():
-	screen.fill( (0,0,0) )
-	pygame.display.flip()
-
 print('Starting instructions')
 show_instructions()
-sleep(5)
-print('Clearing screen')
-clear_screen()
