34a2de8d28c2f5d19e03b6079b9f3662c8960ffc
ChaoticOrb <ChaoticOrb@users.noreply.github.com>
2017-09-05 22:22:16 +0100

    Fixed typo on graphic And added in try except on the flakiest part of the script

--- photobooth.py
+++ photobooth.py
@@ -55,24 +55,28 @@ shutdown_btn.when_held = shutdown
 
 
 def fireUp():
-    leds.off()
-    sleep(1)
-    leds.value = (0,0,0,1) # green led on
-    print('Push the button instruction')
-    displayInstructions(instruction_path + 'button_push.png')
-    big_button.wait_for_press()
-    print('Button pressed')
-    displayInstructions(instruction_path + 'countdown_explanation.png')
-    sleep(5)
-    displayInstructions(instruction_path + 'get_posed.png')
-    sleep(5)
-    clearInstructions()
-    sleep(1)
-    camera.vflip = False # change if camera is mounted upside down
-    camera.resolution = camera.MAX_RESOLUTION
-    camera.start_preview(resolution=(res_w, res_h))
-    print('Camera preview turned on')
-    takePhotos()
+    try:
+        leds.off()
+        sleep(1)
+        leds.value = (0,0,0,1) # green led on
+        print('Push the button instruction')
+        displayInstructions(instruction_path + 'button_push.png')
+        big_button.wait_for_press()
+        print('Button pressed')
+        displayInstructions(instruction_path + 'countdown_explanation.png')
+        sleep(5)
+        displayInstructions(instruction_path + 'get_posed.png')
+        sleep(5)
+        clearInstructions()
+        sleep(1)
+        camera.vflip = False # change if camera is mounted upside down
+        camera.resolution = camera.MAX_RESOLUTION
+        camera.start_preview(resolution=(res_w, res_h))
+        print('Camera preview turned on')
+        takePhotos()
+    except:
+        print('FireUp failed to launch - exiting')
+        sys.exit(1)
 
 def takePhotos():
     try:
