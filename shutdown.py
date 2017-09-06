497704dafbb47bba3a06aa9a5290408968d9e2ad
ChaoticOrb <ChaoticOrb@users.noreply.github.com>
2017-09-06 21:33:05 +0100

    Moved shutdown script to separate file Will boot with cron at boot

---
+++ shutdown.py
@@ -0,0 +1,12 @@
+from gpiozero import Button
+from subprocess import check_call
+from signal import pause
+
+def shutdown():
+    print('Button pushed - shutting down...')
+    check_call(['sudo', 'poweroff'])
+
+shutdown_btn = Button(23, hold_time=5)
+shutdown_btn.when_held = shutdown
+
+pause()
